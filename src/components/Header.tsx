69e6bc87173295f6bf6568d4ea6519a5f29a1113
임재원 <nemo0824@naver.com>
2025-01-25 01:22:28 +0900

    fix: params를 사용하고 currentQuizId를 없앰으로서 상태관리 줄이기 FIRST_QUIZ_ID 로 1 이라는 매직넘버 정의 및 통일 loadingQuiz, fetchQuizData 비동기함수로 실제 통신시 동작할수있도록 변경 paseQuizId에 사용자가 예상치 못한 값 입력시 기본 url로 이동.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,50 +12,69 @@ interface QuizType {
 const Header = () => {
   const navigate = useNavigate();
   const { quizId } = useParams();
-  const [currentQuizId, setCurrentQuizId] = useState<number>(
-    parseInt(quizId || '1', 10)
-  );
+  const FIRST_QUIZ_ID = 1;
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [currentQuiz, setCurrentQuiz] = useState<QuizType | null>(null);
 
-  const fetchQuizData = (id: number) => {
-    const quiz = quizData.find((q) => q.id == id);
-    if (quiz) {
-      setCurrentQuiz(quiz);
-      setError(null);
-    } else {
-      setError('오류 퀴즈가 없어용 ');
-    }
+
+  const fetchQuizData = async (id: number) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const quiz = quizData.find((q) => q.id == id);
+        if (quiz) {
+          setCurrentQuiz(quiz)
+          setError(null)
+          resolve(quiz)
+        } else {
+          setError('오류 퀴즈가 없어용');
+          reject(new Error('퀴즈를 찾을 수 없습니다.'));
+        }
+      }, 0)
+    })
   };
 
   useEffect(() => {
-    setLoading(true);
-    fetchQuizData(currentQuizId);
-    setLoading(false);
-  }, [currentQuizId]);
+    if (location.pathname === "/") return;
+    const parsedQuizId = parseInt(quizId || '', 10);
+    if (isNaN(parsedQuizId) || parsedQuizId < FIRST_QUIZ_ID) {
+      navigate(`/quizzes/${FIRST_QUIZ_ID}`, { replace: true })
+    }
+    const loadingQuiz = async () => {
+      setLoading(true)
+      try {
+        await fetchQuizData(parsedQuizId)
+      } catch {
+        console.error("퀴즈 데이터 통신 에러 발생", error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    loadingQuiz()
+  }, [quizId]);
 
   const handlePrevious = () => {
-    if (currentQuiz && currentQuizId > 1) {
-      const previousQuizId = currentQuizId - 1;
-      setCurrentQuizId(previousQuizId);
+    const parsedQuizId = parseInt(quizId || '', 10);
+    if (currentQuiz && parsedQuizId > FIRST_QUIZ_ID) {
+      const previousQuizId = parsedQuizId - 1;
       navigate(`/quizzes/${previousQuizId}`);
     }
   };
   const handleNext = () => {
+    const parsedQuizId = parseInt(quizId || '', 10);
     if (currentQuiz) {
-      const nextQuizId = currentQuizId + 1;
-      setCurrentQuizId(nextQuizId);
+      const nextQuizId = parsedQuizId + 1;
       navigate(`/quizzes/${nextQuizId}`);
     }
   };
   const handleStartQuiz = () => {
-    navigate('/quizzes/1');
+    navigate(`/quizzes/${FIRST_QUIZ_ID}`);
   };
 
   return (
-    <header className="bg-customGray relative flex h-[60px] items-center p-3">
-      <div className="text-customYellow font-bold absolute left-4 text-xl">
+    <header className="bg-gray-950 relative flex h-[60px] items-center p-3">
+      <div className="text-yellow-950 font-bold absolute left-4 text-xl">
         <p>Javascript Playground</p>
       </div>
       <div className="absolute left-1/2 transform -translate-x-1/2 flex items-center gap-3">
@@ -67,10 +86,10 @@ const Header = () => {
           <>
             <button
               onClick={handlePrevious}
-              disabled={!currentQuizId || currentQuizId <= 1}
+              disabled={!quizId || parseInt(quizId) <= FIRST_QUIZ_ID}
             >
               <FaRegArrowAltCircleLeft
-                className="cursor-pointer text-customYellow"
+                className="cursor-pointer text-yellow-950"
                 size={24}
               />
             </button>
@@ -81,9 +100,9 @@ const Header = () => {
             ) : (
               <p className="text-white">{currentQuiz?.title}</p>
             )}
-            <button onClick={handleNext} disabled={!currentQuizId}>
+            <button onClick={handleNext} disabled={!quizId}>
               <FaRegArrowAltCircleRight
-                className="cursor-pointer text-customYellow"
+                className="cursor-pointer text-yellow-950"
                 size={24}
               />
             </button>
