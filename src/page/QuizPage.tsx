3507e2f653053a0292d63383e141be532ecb5b52
임재원 <nemo0824@naver.com>
2025-01-27 15:49:27 +0900

    feature: 퀴즈 editor codeMirror 사용, 컴파일 구조 설정,

--- src/page/QuizPage.tsx
+++ src/page/QuizPage.tsx
@@ -1,10 +1,11 @@
 import { QuizEditor } from '../components/QuizEditor';
 import { QuizVisualization } from '../components/QuizVisualization';
-
+import { Quiz } from '@/components/Quiz';
 export const QuizPage = () => {
   return (
     <div className="flex flex-1">
-      <div className="w-1/2">
+      <div className="w-1/2 m-2">
+        <Quiz></Quiz>
         <QuizEditor></QuizEditor>
       </div>
       <div className="w-1/2">
