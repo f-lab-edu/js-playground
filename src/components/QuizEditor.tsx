70c2a324abb436d5076bd1042f5e0361a3c0bb91
임재원 <nemo0824@naver.com>
2025-02-04 21:25:01 +0900

    feature: 퀴즈에대한 정답판별 / 힌트요소 렌더링

--- src/components/QuizEditor.tsx
+++ src/components/QuizEditor.tsx
@@ -1,22 +1,32 @@
 import { useQuizResultStore } from '@/store/useQuiz';
 import { javascript } from '@codemirror/lang-javascript';
 import CodeMirror, { lineNumbers, oneDark } from '@uiw/react-codemirror';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export const QuizEditor = () => {
   const { setUserCode } = useQuizResultStore();
-  const [value] = useState(`function solution(){
+  const { quizId } = useParams();
+  const [codeValue, setCodeValue] = useState(`function solution(){
   
 }`);
   const handleCodeChange = (code: string) => {
     setUserCode(code);
   };
 
+  useEffect(() => {
+    setUserCode('');
+    setCodeValue(`function solution(){
+  
+}`);
+  }, [quizId]);
+
   return (
     <div>
       <CodeMirror
-        className="mt-1"
-        value={value}
+        key={quizId}
+        className="mt-2"
+        value={codeValue}
         theme={oneDark}
         height="500px"
         extensions={[javascript({ jsx: true }), lineNumbers()]}
